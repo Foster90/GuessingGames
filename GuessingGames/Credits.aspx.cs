72d4ea1c62d6938b12ae2b85da1122c8dd8b590e
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-09-30 20:13:01 +0100

    Update credits

--- GuessingGames/Credits.aspx.cs
+++ GuessingGames/Credits.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 namespace GuessingGames
 {
@@ -11,7 +12,8 @@ namespace GuessingGames
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            HtmlGenericControl Body = (HtmlGenericControl)Master.FindControl("Body");
+            Body.Attributes.Add("style", "background-color: #0d1b2a");
         }
     }
 }
